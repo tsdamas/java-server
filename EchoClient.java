27966401154e7c5e3fb4261e00d258c2ffaa16f2
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-28 11:01:45 -0800

    create socket and print ports

--- EchoClient.java
+++ EchoClient.java
@@ -10,6 +10,13 @@ public class EchoClient {
 
         try {
             //Create a connection to the server by creating a socket
+            Socket newSock = new Socket("127.0.0.1", port);
+
+            //Get the remote port open on the server
+            System.out.println("The open port on the server is " + newSock.getPort());
+
+            //Get the local port on the client
+            System.out.println("The local port is " + newSock.getLocalPort()); 
             
         } catch (IOException ioe) {
             System.out.println("Problem connecting" + ioe.getMessage()); 
