71747092f2d77016d48e0a28503cc9af5c2a7952
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-28 10:36:52 -0800

    change to start from client

--- EchoClient.java
+++ EchoClient.java
@@ -7,7 +7,11 @@ import java.util.Scanner;
 public class EchoServer {
     public static void main(String[] args) {
         int port = 4243; 
+        if(args.length > 0) port = Integer.parseInt(args[0]); 
+
         try {
+            //Create a connection to the server by creating a socket
+            Socket newSock = new Socket(port);  
             
         } catch (IOException ioe) {
             System.out.println("Problem connecting" + ioe.getMessage()); 
