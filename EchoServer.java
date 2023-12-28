7ce512189bfce6df5941b5d3dc18177d3519c699
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-28 10:23:26 -0800

    add echo server and try catch block

---
+++ EchoServer.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        int port = 4243; 
+        try {
+            
+        } catch (IOException ioe) {
+            System.out.println("Problem connecting" + ioe.getMessage()); 
+        }
+    }
+}
