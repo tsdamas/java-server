2a7336fc31f5f0450131388b9f75052c141cddaa
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-28 11:19:00 -0800

    create socket and print port

--- EchoServer.java
+++ EchoServer.java
@@ -10,6 +10,14 @@ public class EchoServer {
 
         if(args.length > 0) port = Integer.parseInt(args[0]); 
         try {
+            //Server socket which does not need of ip address cause we use the machine's address
+            ServerSocket sock = new ServerSocket(port);
+
+            //Show connection by printing port
+            System.out.println("The port used is " + sock.getLocalPort()); 
+
+            
+
             
         } catch (IOException ioe) {
             System.out.println("Problem connecting" + ioe.getMessage()); 
